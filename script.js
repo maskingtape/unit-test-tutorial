f7fefc6a903fd0f5afaef570178939da98d6a2cc
Dan Eastwell <daneastwell@gmail.com>
2013-04-03 15:14:19 +0100

    Add getURIParam stub and tests.

---
+++ script.js
@@ -0,0 +1,14 @@
+/*global jQuery*/
+
+var mySite = (function(window, $){
+  "use strict";
+  
+  var self = {
+    getURIParam : function getURIParam(uri, param){
+    
+    }
+  };
+  
+  return self;
+  
+})(window, jQuery);
--- test/test.js
+++ test/test.js
@@ -1,1 +1,41 @@
-(function(globals){  "use strict";  var assert    = globals.chai.assert,      expect    = globals.chai.expect,      jQuery    = globals.jQuery;        describe('#getURIParam', function(){      it('should return null if there are no parameters in the URI', function(){      assert.ok(false, 'test not implemented yet');    });        it('should return the correct parameter if there is only one parameter', function(){      assert.ok(false, 'test not implemented yet');    });        it('should return the correct parameter if there is more than one parameter', function(){      assert.ok(false, 'test not implemented yet');    });        it('should return null if the parameter is not present', function(){      assert.ok(false, 'test not implemented yet');    });        it('should return the correct parameter if there is also a hash value', function(){      assert.ok(false, 'test not implemented yet');    });      });  })(this);
+(function(globals){
+  "use strict";
+  var assert    = globals.chai.assert,
+      expect    = globals.chai.expect,
+      jQuery    = globals.jQuery;
+      
+  describe('#getURIParam', function(){
+  
+    it('should return null if there are no parameters in the URI', function(){
+      var url = 'http://www.example.com/index.html',
+          param = 'test';
+      expect(mySite.getURIParam(url, param)).to.be.null;
+    });
+    
+    it('should return the correct parameter if there is only one parameter', function(){
+      var url = 'http://www.example.com/index.html?test=3',
+          param = 'test';
+      expect(mySite.getURIParam(url, param)).to.equal('3');
+    });
+    
+    it('should return the correct parameter if there is more than one parameter', function(){
+      var url = 'http://www.example.com/index.html?item=003&name=dan&test=3',
+          param = 'test';
+      expect(mySite.getURIParam(url, param)).to.equal('3');
+    });
+    
+    it('should return null if the parameter is not present', function(){
+      var url = 'http://www.example.com/index.html?item=003&name=dan&amount=3',
+          param = 'test';
+      expect(mySite.getURIParam(url, param)).to.be.null;
+    });
+    
+    it('should return the correct parameter if there is also a hash value', function(){
+      var url = 'http://www.example.com/index.html?item=003&name=dan&test=3#inPageLink',
+          param = 'test';
+      expect(mySite.getURIParam(url, param)).to.equal('3');
+    });
+    
+  });
+  
+})(this);
