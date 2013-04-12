4d26aa954ef35ea5a8352b0993ec73b275ccb663
Dan Eastwell <daneastwell@gmail.com>
2013-04-12 14:45:07 +0100

    Add getURIParam function

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ var mySite = (function(window, $){
   
   var self = {
     getURIParam : function getURIParam(uri, param){
+      var search = uri.split('?')[1],
+          params = [];
+
+      if(typeof search === 'undefined'){
+        return null;
+      }
+      
+      params = search.split('&');
+      
+      if(params.length === 0){
+        return null;
+      }
+      
+      for(var i=0;i<params.length;i++){
+        var arr = params[i].split('='),
+            key    = arr[0],
+            value  = arr[1];
+        if(key === param){
+          return value.split('#')[0];
+        }
+      }
+      
+      return null;
     
     }
   };
